fecc912abbeb04634e2be10bdef9acd830f08b68
Timothy Hardy <timothyhardyg@gmail.com>
2025-12-09 11:00:34 -0700

    Cleaning repository

--- cleaning.R
+++
@@ -1,9 +0,0 @@
-# Clean data provided by {qatarcars}. No cleaning was necessary.
-qatarcars <- readr::read_csv("https://raw.githubusercontent.com/profmusgrave/qatarcars/refs/heads/main/inst/extdata/qatarcars.csv")
-
-# Or alternatively, using the {qatarcars} package:
-#
-# install.packages("qatarcars")
-# library(qatarcars)
-# data(qatarcars, package = "qatarcars")
-
--- data_acq.py
+++ data_acq.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 pydytuesday.get_date('2025-12-09')
 
-df = pd.read_csv("qatarcars.csv")
 
 
 
